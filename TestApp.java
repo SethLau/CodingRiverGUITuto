39cdcf85444411798ecfa1f5a09bb7b066ebcd40
SethLau <sethyomba488@gmail.com>
2024-07-18 14:15:23 +0200

    Premiers code

---
+++ TestApp.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+
+
+
+public class TestApp {
+
+    
+    public static void main(String[] args) {
+        String str = JOptionPane.showInputDialog("How are you?");
+        System.out.println(str);
+    }
+    
+}
